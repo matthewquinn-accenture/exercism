f26eb6715367cd5d50b40784e80e0fb99034bd87
matthew quinn <matthewquinn.personal@gmail.com>
2017-09-12 19:04:08 -0400

    rewrote to include HashSet and added characters to that

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        return input.equals("") || !input.contains("x") ? false : true;
+        Set<Character> alphabet = new HashSet<>();
+
+        String lowerCaseInput = input.toLowerCase();
+        String noBlanksInput = lowerCaseInput.replaceAll("\\s+", "");
+        char[] inputChars = noBlanksInput.toCharArray();
+
+        for (char character : inputChars) {
+            alphabet.add(character);
+            if(alphabet.size() == 26){
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
