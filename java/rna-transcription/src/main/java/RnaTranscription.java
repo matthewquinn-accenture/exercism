07314b0e36e9a198caf1d56839fb8adcc2c0bb70
matthew quinn <matthewquinn.personal@gmail.com>
2017-09-12 14:13:51 -0400

    created Map dnaStrands to store DNA:RNA pairs

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        return "G";
-    }
+        Map<String, String> dnaStrands = new HashMap();
+        dnaStrands.put("G", "C");
+        dnaStrands.put("C", "G");
 
+        return dnaStrands.get(dnaStrand);
+    }
 }
