0239d5978be2c8110c0b799e8c7f45507bfc70ab
matthew quinn <matthewquinn.personal@gmail.com>
2017-09-14 14:20:55 -0400

    refactored out constants

--- java/space-age/src/main/java/SpaceAge.java
+++ java/space-age/src/main/java/SpaceAge.java
@@ -1,6 +1,13 @@
 class SpaceAge {
 
-    public static final int earthSeconds = 31_557_600;
+    public static final int EARTH_SECONDS_PER_YEAR = 31_557_600;
+    public static final double MERCURY_TIME_PER_EARTH_YEAR = 0.2408467;
+    public static final double VENUS_TIME_PER_EARTH_YEAR = 0.61519726;
+    public static final double MARS_TIME_PER_EARTH_YEAR = 1.8808158;
+    public static final double JUPITER_TIME_PER_EARTH_YEAR = 11.862615;
+    public static final double SATURN_TIME_PER_EARTH_YEAR = 29.447498;
+    public static final double URANUS_TIME_PER_EARTH_YEAR = 84.016846;
+    public static final double NEPTUNE_TIME_PER_EARTH_YEAR = 164.79132;
     private double seconds;
 
     SpaceAge(double seconds) {
@@ -12,35 +19,35 @@ class SpaceAge {
     }
 
     double onEarth() {
-        return seconds / earthSeconds;
+        return seconds / EARTH_SECONDS_PER_YEAR;
     }
 
     double onMercury() {
-        return seconds / (0.2408467 * earthSeconds);
+        return seconds / (MERCURY_TIME_PER_EARTH_YEAR * EARTH_SECONDS_PER_YEAR);
     }
 
     double onVenus() {
-        return seconds / (0.61519726 * earthSeconds);
+        return seconds / (VENUS_TIME_PER_EARTH_YEAR * EARTH_SECONDS_PER_YEAR);
     }
 
     double onMars() {
-        return seconds / (1.8808158 * earthSeconds);
+        return seconds / (MARS_TIME_PER_EARTH_YEAR * EARTH_SECONDS_PER_YEAR);
     }
 
     double onJupiter() {
-        return seconds / (11.862615 * earthSeconds);
+        return seconds / (JUPITER_TIME_PER_EARTH_YEAR * EARTH_SECONDS_PER_YEAR);
     }
 
     double onSaturn() {
-        return seconds / (29.447498 * earthSeconds);
+        return seconds / (SATURN_TIME_PER_EARTH_YEAR * EARTH_SECONDS_PER_YEAR);
     }
 
     double onUranus() {
-        return seconds / (84.016846 * earthSeconds);
+        return seconds / (URANUS_TIME_PER_EARTH_YEAR * EARTH_SECONDS_PER_YEAR);
     }
 
     double onNeptune() {
-        return seconds / (164.79132 * earthSeconds);
+        return seconds / (NEPTUNE_TIME_PER_EARTH_YEAR * EARTH_SECONDS_PER_YEAR);
     }
 
 }
