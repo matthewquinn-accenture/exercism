7f00650b3f178e6871cfec0a1454ee4075426ac9
matthew quinn <matthewquinn.personal@gmail.com>
2017-09-15 10:45:54 -0400

    created a Map of char[]:Integer to hold scrabbleScores for each letter

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Scrabble {
 
+    private Map<String, Integer> scrabbleScoreCard = new HashMap<>();
+    char[] onePointLetters = {'A','E','I','O','U',
+            'L','N','R','S','T'};
+    char[] twoPointLetters = {'D','G'};
+    char[] threePointLetters = {'B','C','M','P'};
+    char[] fourPointLetters = {'F','H','V','W','Y'};
+    char[] fivePointLetters = {'K'};
+    char[] eightPointLetters = {'J','X'};
+    char[] tenPointLetters = {'Q','Z'};
+
+    public Scrabble(Map<char[], Integer> scrabbleScoreCard) {
+        scrabbleScoreCard.put(onePointLetters, 1);
+        scrabbleScoreCard.put(twoPointLetters, 2);
+        scrabbleScoreCard.put(threePointLetters, 3);
+        scrabbleScoreCard.put(fourPointLetters, 4);
+        scrabbleScoreCard.put(fivePointLetters, 5);
+        scrabbleScoreCard.put(eightPointLetters, 8);
+        scrabbleScoreCard.put(tenPointLetters, 10);
+    }
+
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String upperCaseWord = word.toUpperCase();
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return 0;
     }
 
 }
