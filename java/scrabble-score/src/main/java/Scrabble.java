a574f423c2f07a2c629f659cd27c3b5ecf8bff02
matthew quinn <matthewquinn.personal@gmail.com>
2017-09-18 15:01:58 -0400

    changed char[] to List<Character>, all passing tests

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,19 +1,31 @@
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 class Scrabble {
 
-    private Map<String, Integer> scrabbleScoreCard = new HashMap<>();
-    char[] onePointLetters = {'A','E','I','O','U',
-            'L','N','R','S','T'};
-    char[] twoPointLetters = {'D','G'};
-    char[] threePointLetters = {'B','C','M','P'};
-    char[] fourPointLetters = {'F','H','V','W','Y'};
-    char[] fivePointLetters = {'K'};
-    char[] eightPointLetters = {'J','X'};
-    char[] tenPointLetters = {'Q','Z'};
+    private String word;
 
-    public Scrabble(Map<char[], Integer> scrabbleScoreCard) {
+    private Map<List, Integer> scrabbleScoreCard = new LinkedHashMap<>();
+
+    private List<Character> onePointLetters = Arrays.asList('A','E','I','O','U',
+            'L','N','R','S','T');
+    private List<Character> twoPointLetters = Arrays.asList('D','G');
+    private List<Character> threePointLetters = Arrays.asList('B','C','M','P');
+    private List<Character> fourPointLetters = Arrays.asList('F','H','V','W','Y');
+    private List<Character> fivePointLetters = Arrays.asList('K');
+    private List<Character> eightPointLetters = Arrays.asList('J','X');
+    private List<Character> tenPointLetters = Arrays.asList('Q','Z');
+    private List<Character> lettersToBeScored;
+
+    private char[] lettersPlayed;
+    
+    private int score = 0;
+
+    Scrabble(String word) {
+        this.word = word.toUpperCase();
+
         scrabbleScoreCard.put(onePointLetters, 1);
         scrabbleScoreCard.put(twoPointLetters, 2);
         scrabbleScoreCard.put(threePointLetters, 3);
@@ -23,12 +35,19 @@ class Scrabble {
         scrabbleScoreCard.put(tenPointLetters, 10);
     }
 
-    Scrabble(String word) {
-        String upperCaseWord = word.toUpperCase();
-    }
-
     int getScore() {
-        return 0;
+        lettersPlayed = word.toCharArray();
+
+        for (int i = 0; i < lettersPlayed.length; i++) {
+            for(Map.Entry<List, Integer> entry : scrabbleScoreCard.entrySet()){
+                lettersToBeScored = entry.getKey();
+                Integer pointsPerLetter = entry.getValue();
+                if(lettersToBeScored.contains(lettersPlayed[i])) {
+                    score += pointsPerLetter;
+                }
+            }
+        }
+        return score;
     }
 
 }
