e489d5df28a23a9fa7b5922c8162df64813c5390
matthew quinn <matthewquinn.personal@gmail.com>
2017-09-19 14:02:30 -0400

    method extraction refactor

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -3,20 +3,32 @@ class RaindropConverter {
     private String rainDrop = "";
 
     String convert(int number) {
-        boolean numberHas3PrimeFactor = number % 3 == 0;
-        boolean numberHas5PrimeFactor = number % 5 == 0;
-        boolean numberHas7PrimeFactor = number % 7 == 0;
-        boolean numberHasNoPrimeFactor = !numberHas3PrimeFactor && !numberHas5PrimeFactor && !numberHas7PrimeFactor;
 
-        if (numberHas3PrimeFactor) rainDrop = "Pling";
+        if (isNumberAPrimeFactorOf3(number)) rainDrop = "Pling";
 
-        if (numberHas5PrimeFactor) rainDrop += "Plang";
+        if (isNumberAPrimeFactorOf5(number)) rainDrop += "Plang";
 
-        if (numberHas7PrimeFactor) rainDrop += "Plong";
+        if (isNumberAPrimeFactorOf7(number)) rainDrop += "Plong";
 
-        if(numberHasNoPrimeFactor) rainDrop = String.valueOf(number);
+        if(isNumberANonPrime(number)) rainDrop = String.valueOf(number);
 
         return rainDrop;
+    }
+
+    private boolean isNumberAPrimeFactorOf3(int number) {
+        return number % 3 == 0;
+    }
+
+    private boolean isNumberAPrimeFactorOf5(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean isNumberAPrimeFactorOf7(int number) {
+        return number % 7 == 0;
+    }
+
+    private boolean isNumberANonPrime(int number) {
+        return !isNumberAPrimeFactorOf3(number) && !isNumberAPrimeFactorOf5(number) && !isNumberAPrimeFactorOf7(number);
     }
 
 }
