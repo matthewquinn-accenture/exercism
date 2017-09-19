0a9e4a0d035f4a24b4beb4271c3791fc4c0a9811
matthew quinn <matthewquinn.personal@gmail.com>
2017-09-19 12:28:06 -0400

    refactored code

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -2,14 +2,22 @@ class RaindropConverter {
 
     String convert(int number) {
         String rainDrop = "";
+        boolean numberHas7PrimeFactor = number % 7 == 0;
+        boolean numberHas5PrimeFactor = number % 5 == 0;
+        boolean numberHas3PrimeFactor = number % 3 == 0;
 
-        if (number % 7 == 0) {
+        if (numberHas7PrimeFactor) {
             rainDrop = "Plong";
-        } else if (number % 5 == 0){
+        }
+
+        if (numberHas5PrimeFactor){
             rainDrop = "Plang";
-        } else {
+        }
+
+        if (numberHas3PrimeFactor) {
             rainDrop = "Pling";
         }
+
         return rainDrop;
     }
 
