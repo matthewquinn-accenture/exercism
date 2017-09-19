a5fe7015aeb112d689bd52fe2bacdb376e627192
matthew quinn <matthewquinn.personal@gmail.com>
2017-09-19 14:58:12 -0400

    initial commit, created DifferenceOfSquaresCalculator class

---
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,15 @@
+public class DifferenceOfSquaresCalculator {
+
+
+    public int computeSquareOfSumTo(int i) {
+        return 0;
+    }
+
+    public int computeSumOfSquaresTo(int i) {
+        return 0;
+    }
+
+    public int computeDifferenceOfSquares(int i) {
+        return 0;
+    }
+}
