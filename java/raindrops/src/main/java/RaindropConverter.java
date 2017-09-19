4017398260069e049e2b083e1950cf34058ecd57
matthew quinn <matthewquinn.personal@gmail.com>
2017-09-19 13:39:41 -0400

    refactoring

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -2,26 +2,18 @@ class RaindropConverter {
 
     String convert(int number) {
         String rainDrop = "";
-        boolean numberHas7PrimeFactor = number % 7 == 0;
-        boolean numberHas5PrimeFactor = number % 5 == 0;
         boolean numberHas3PrimeFactor = number % 3 == 0;
-        boolean numberHasNoPrimeFactor = number == 1 || number == 52 || number == 12121;
+        boolean numberHas5PrimeFactor = number % 5 == 0;
+        boolean numberHas7PrimeFactor = number % 7 == 0;
+        boolean numberHasNoPrimeFactor = !numberHas3PrimeFactor && !numberHas5PrimeFactor && !numberHas7PrimeFactor;
 
-        if (numberHas3PrimeFactor) {
-            rainDrop = "Pling";
-        }
+        if (numberHas3PrimeFactor) rainDrop = "Pling";
 
-        if (numberHas5PrimeFactor){
-            rainDrop += "Plang";
-        }
+        if (numberHas5PrimeFactor) rainDrop += "Plang";
 
-        if (numberHas7PrimeFactor) {
-            rainDrop += "Plong";
-        }
+        if (numberHas7PrimeFactor) rainDrop += "Plong";
 
-        if(numberHasNoPrimeFactor){
-            rainDrop = String.valueOf(number);
-        }
+        if(numberHasNoPrimeFactor) rainDrop = String.valueOf(number);
 
         return rainDrop;
     }
